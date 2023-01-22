5cd96b1ec0fa2dedf2545ac1aac0dba3033f3578
serkancagman <serkancagmann@gmail.com>
2023-01-22 21:30:20 +0300

    Add:cdn

--- main.js
+++ main.js
@@ -2658,7 +2658,7 @@ window.onload = () => {
   dynamicallyLoadScript(
     "http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"
   );
-  loadStyle("https://cdn.jsdelivr.net/gh/serkancagman/wheel-test/style.css");
+  loadStyle("https://cdn.jsdelivr.net/gh/serkancagman/wheel-testt/style.css");
   createWheelButton();
   createLottie();
 
